731eff36fb595fb8609ed145043e920919ee177f
aythameitor <aythamicm@gmail.com>
2023-01-26 20:42:38 +0000

    final dia 26/01/23

--- partes/footer.php
+++ partes/footer.php
@@ -1,10 +1,10 @@
-<div style="position:absolute;bottom:0;display:flex;justify-content:space-around;width:100%; background-color:black; color:white;">
+<!--<div style="bottom:0;display:flex;justify-content:space-around;width:100%; background-color:black; color:white;">
     <div>
         <p>Aythami miguel cabrera mayor</p>
     </div>
     <div style="align-items:center">
         <p>DSW 2023</p>
     </div>
-</div>
+</div>-->
 </body>
 </html>
