c3f7c5b5f5e8c3f945cfc4a67a2ced628ef05840
aythameitor <aythamicm@gmail.com>
2023-02-02 20:59:41 +0000

    push para heroku

--- config.php
+++ config.php
@@ -4,10 +4,10 @@
 
     return [
         'db' => [
-            'host' => 'mysql:host=localhost',
-            'user' => 'administradorRestaurante',
-            'pass' => 'admin',
-            'name' => 'reservarestaurante',
+            'host' => $_ENV['DB_HOST'],
+            'user' => $_ENV['DB_USER'],
+            'pass' => $_ENV['DB_PASS'],
+            'name' => $_ENV['DB_DB'],
             'options' => [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
             ]
